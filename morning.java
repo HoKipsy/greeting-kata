7fa97ecdcae762d7ae8728920be29e2a30f73ca3
HoKipsy <remenyireka2000@gmail.com>
2018-02-28 12:38:05 +0100

    Update morning.java
    More methods to use - 1 line before, 1 line after

--- morning.java
+++ morning.java
@@ -1,24 +1,28 @@
 public class Morning
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) {        //the main thing which greets the person
 		System.out.println(greeting("Sasha"));
 	}
 	
-    public static String greeting(String name) {
-        String kiirando = "";
-        if (name.length() == 0){
-            kiirando = csillag() + "\n" + "Happy Easter, my friend!" + "\n" + csillag();
-        } else {
-            kiirando = csillag() + "\n" + "Happy Easter, " + name + "!" + "\n" + csillag();
-        }
+    public static String greeting(String name) {    //the greeting method
+        if (name.length() == 0) {
+            name = "my friend";
+        } else {};
+        String msgLine = "*Happy Easter, " + name + "!*";
+        String kiirando = lotChars("*", 80) + "\n" + msgLine + "\n" + lotChars("*", 80);
         return kiirando;
     }
     
-    public static String csillag() {
-        String csillagok = "";
-        for(int i = 0; i < 80; i++) {
-            csillagok = csillagok + "*";
+    public static String lotChars(String chars, int number) {
+        String lotChar = "";
+        for(int i = 0; i < number; i++) {
+            lotChar = lotChar + chars;
         };
-        return csillagok;
+        return lotChar;
+    }
+
+    public static int howMany(String current) {        //this 1 calculates the number of spaces we need
+        int neededSpaces = (80 - current.length()) / 2;
+        return neededSpaces;
     }
 }
