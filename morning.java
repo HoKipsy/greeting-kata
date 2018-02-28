5ef5e08135e77772a5da48ba9070edc2283f1381
HoKipsy <remenyireka2000@gmail.com>
2018-02-28 09:59:35 +0100

    Update morning.java

--- morning.java
+++ morning.java
@@ -3,4 +3,8 @@ public class morning {
     public static void main(String[] args) {
         System.out.println("Good morning!");
     }
+    
+    pubic static void greeting(String name) {
+        System.out.println("Good morning " + name + "!");
+    }
 }
